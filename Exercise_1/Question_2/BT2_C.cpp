11f36330718f3fdf454aacead08b866de15dc3c1
duvohuu <du.vohuudu@gmail.com>
2025-09-22 23:34:57 +0700

    Update Bai1 & Bai2 & Bai3

---
+++ Exercise_1/Question_2/BT2_C.cpp
@@ -0,0 +1,21 @@
+﻿#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat source = (Mat_<uint8_t>(4, 4) << 50, 20, 30, 50,
+        80, 50, 100, 110,
+        120, 160, 50, 150,
+        220, 230, 240, 250);
+    Mat dst;
+    double thresh = 0, maxValue = 255;
+    long double thres = threshold(source, dst, thresh, maxValue, THRESH_OTSU);
+
+    cout << "Image matrix = " << endl << "" << source << endl;
+    cout << "Otsu Threshold T = " << thres << endl;
+    cout << "Otsu Threshold result" << endl << "" << dst << endl;
+
+    waitKey(0);
+    return 0;
+}
